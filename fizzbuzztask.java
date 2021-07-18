674910595177aa160358dbf0e8f9da744c7244fc
102092 <quaint3@gmail.com>
2021-07-18 23:36:21 +0900

    Solve fizzbuzzTask

--- fizzbuzztask.java
+++ fizzbuzztask.java
@@ -8,11 +8,44 @@
 ***/
 interface FizzBuzz {
     void print(int from, int to);
+    boolean fizz(int number);
+    boolean buzz(int number);
 }
 
 
 class ConsoleBasedFizzBuzz implements FizzBuzz {
     // TODO implement FizzBuzz interface
+    private final String fizz = "Fizz";
+    private final String buzz = "Buzz";
+
+    @java.lang.Override
+    public void print(int from, int to) {
+        if (from < 1 || to > 100) {
+            return;
+        }
+
+        for (int i = from; i < to ; i++) {
+            if (fizz(i) && buzz(i)) {
+                System.out.println(fizz + buzz);
+            } else if(fizz(i)) {
+                System.out.println(fizz);
+            } else if(buzz(i)) {
+                System.out.println(buzz);
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    @java.lang.Override
+    public boolean fizz(int number) {
+        return number % 3 == 0;
+    }
+
+    @java.lang.Override
+    public boolean buzz(int number) {
+        return number % 5 == 0;
+    }
 }
 
 
@@ -20,4 +53,8 @@ class Main {
     // TODO use a main method
     // TODO print fizz buzz from 1 to max
     // TODO max is passed from the console
+    public static void main(String[] args) {
+        ConsoleBasedFizzBuzz consoleBasedFizzBuzz = new ConsoleBasedFizzBuzz();
+        consoleBasedFizzBuzz.print(1, 100);
+    }
 }
